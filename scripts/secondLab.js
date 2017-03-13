c5c3b682bcb221682dd08389925aeef64597b370
freyk3 <freyk3@gmail.com>
2017-03-13 19:50:42 +0300

    lab 2 beta

--- scripts/secondLab.js
+++ scripts/secondLab.js
@@ -20,7 +20,48 @@ var secTempIsCorrect1,
     secTempIsCorrect4,
     viderjTimeIsCorrect,
     gomoDavlenIsCorrect;
-var finishTemp;
+var finishTemp,
+    tempSec1,
+    tempSec2,
+    tempSec3,
+    tempSec4,
+    viderjTime,
+    gomoDavlen;
+var appIsStart = false;
+var processIsStart = false;
+// Переменные для таймера
+function trim(string) { return string.replace (/\s+/g, " ").replace(/(^\s*)|(\s*)$/g, ''); }
+var init=0;
+var startDate;
+var clocktimer;
+var timer = 0;
+
+var fs = require('fs');
+var pathToStorage = require('path');
+var Docxtemplater = require('docxtemplater');
+var JSZip = require('jszip');
+
+var usersFile = 'users.json';
+var pathToUsersFile = pathToStorage.join(nw.App.dataPath, usersFile);
+var contents = fs.readFileSync(pathToUsersFile, 'utf8');
+var users = JSON.parse(contents);
+var user;
+for(var key in users)
+{
+    if (!users.hasOwnProperty(key)) continue;
+    var tempUser = users[key];
+
+    if(tempUser.login == sessionStorage.user)
+    {
+        user = tempUser;
+        break;
+    }
+}
+
+var reportObj = {};
+reportObj.errors = [];
+reportObj.log = [];
+reportObj.user = user;
 
 var mainPipes;
 for(var i=1; i<14; i++)
@@ -120,8 +161,6 @@ function startAgain() {
 
 var templateNumb = randomInteger(0,3);
 mainTemplate = templates[templateNumb];
-document.getElementById('temperatura1').innerHTML = mainTemplate.enterTemper;
-document.getElementById('jirnost1').innerHTML = mainTemplate.enterJir;
 console.log('Номер шаблона: '+templateNumb);
 var errorNumb = randomInteger(0,3);
 if(errorNumb == 0) //для обеспечения 1 к 4 появление ошибки
@@ -132,6 +171,7 @@ console.log('Наличие ошибки: '+errorExist);
 
 
 function pipeAnimation() {
+    processIsStart = true;
     var pipe = mainPipes[iterator];
     var internalPipe = pipe.firstElementChild;
     var lenght = internalPipe.getAttribute('data-lenght');
@@ -168,6 +208,7 @@ function pipeAnimation() {
             }
             else
             {
+                processIsStart = false;
                 if(appIsFinished == false)
                 {
                     clearPipes();
@@ -280,6 +321,8 @@ function randomInteger(min, max) {
 
 
 function chooseSec1() {
+    if(!appIsStart)
+        return;
     var temp = prompt('Введите температуру в °С:','');
     if(temp == '')
         return;
@@ -287,13 +330,18 @@ function chooseSec1() {
     if(temp >= mainTemplate.temperSec1Min && temp <= mainTemplate.temperSec1Max)
     {
         secTempIsCorrect1 = true;
+        tempSec1 = temp;
+        createLog('Верно выбрана температура в секции 1: '+temp+'°С');
     }
     else
     {
         secTempIsCorrect1 = false;
+        createError('Неверно выбрана температура в секции 1: '+temp+'°С');
     }
 }
 function chooseSec2() {
+    if(!appIsStart)
+        return;
     var temp = prompt('Введите температуру в °С','');
     if(temp == '')
         return;
@@ -301,13 +349,18 @@ function chooseSec2() {
     if(temp >= mainTemplate.temperSec2Min && temp <= mainTemplate.temperSec2Max)
     {
         secTempIsCorrect2 = true;
+        tempSec2 = temp;
+        createLog('Верно выбрана температура в секции 2: '+temp+'°С');
     }
     else
     {
         secTempIsCorrect2 = false;
+        createError('Неверно выбрана температура в секции 2: '+temp+'°С');
     }
 }
 function chooseSec3() {
+    if(!appIsStart)
+        return;
     var temp = prompt('Введите температуру в °С:','');
     if(temp == '')
         return;
@@ -315,10 +368,13 @@ function chooseSec3() {
     if(temp >= mainTemplate.temperSec3Min && temp <= mainTemplate.temperSec3Max)
     {
         secTempIsCorrect3 = true;
+        tempSec3 = temp;
+        createLog('Верно выбрана температура в секции 3: '+temp+'°С');
     }
     else
     {
         secTempIsCorrect3 = false;
+        createError('Неверно выбрана температура в секции 3: '+temp+'°С');
     }
 
     var time = prompt('Введите время выдерживания в секундах:','');
@@ -328,13 +384,18 @@ function chooseSec3() {
     if(time == mainTemplate.viderjTime)
     {
         viderjTimeIsCorrect = true;
+        viderjTime = time;
+        createLog('Верно выбрано время выдержки в выдерживателе: '+time+' сек');
     }
     else
     {
         viderjTimeIsCorrect = false;
+        createError('Неверно выбрано время выдержки в выдерживателе: '+time+' сек');
     }
 }
 function chooseSec4() {
+    if(!appIsStart)
+        return;
     var temp = prompt('Введите температуру в °С','');
     if(temp == '')
         return;
@@ -342,13 +403,18 @@ function chooseSec4() {
     if(temp >= mainTemplate.temperSec4Min && temp <= mainTemplate.temperSec4Max)
     {
         secTempIsCorrect4 = true;
+        tempSec4 = temp;
+        createLog('Верно выбрана температура в секции 4: '+temp+'°С');
     }
     else
     {
         secTempIsCorrect4 = false;
+        createError('Неверно выбрана температура в секции 4: '+temp+'°С');
     }
 }
 function chooseGomog() {
+    if(!appIsStart)
+        return;
     var davlen = prompt('Введите давление в МПа:','');
     if(davlen == '')
         return;
@@ -356,14 +422,22 @@ function chooseGomog() {
     if(davlen >= mainTemplate.gomoDavlenMin && davlen <= mainTemplate.gomoDavlenMax)
     {
         gomoDavlenIsCorrect = true;
+        gomoDavlen = davlen;
+        createLog('Верно выбрано давление в гомогенизаторе: '+davlen+' МПа');
     }
     else
     {
         gomoDavlenIsCorrect = false;
+        createError('Неверно выбрано давление в гомогенизаторе: '+davlen+' МПа');
     }
 }
 
 function checkAll() {
+    if(processIsStart)
+        return;
+    if(!appIsStart)
+        return;
+
     if(secTempIsCorrect1 == undefined)
     {
         alert('Не установлена температура в секции 1');
@@ -434,10 +508,125 @@ function checkAll() {
 }
 
 function finishLab2() {
+    createLog('Работа окончена');
+    createReport();
     alert('Работа окончена! Отчет можно найти в разделе отчетов');
     document.location.href='dashboard.html';
 }
 
 function tankInfo1() {
-    alert();
+    if(!appIsStart)
+        return;
+    alert('Температура: '+mainTemplate.enterTemper+'\nЖирность: '+mainTemplate.enterJir+'\n\nНеобходимо получить\n\nТемпература: 4±2 °С\nЖирность: '+mainTemplate.vihodJir);
+}
+
+function createError(msg) {
+    var errorObj = {};
+    errorObj.time = timer;
+    errorObj.message = msg;
+    reportObj.errors.push(errorObj);
+}
+function createLog(msg) {
+    var logObj = {};
+    logObj.time = timer;
+    logObj.message = msg;
+    reportObj.log.push(logObj);
+}
+
+function createReport() {
+    var content = fs.readFileSync("files/report_template_lab2.docx", "binary");
+    var zip = new JSZip(content);
+    var doc= new Docxtemplater().loadZip(zip);
+
+    var date = new Date;
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    if(month<10)
+        month = '0'+month;
+    if(day<10)
+        day = '0'+day;
+
+    var nowDate = day +'.'+month+'.'+year;
+    doc.setData({
+        "errors": reportObj.errors,
+        "log": reportObj.log,
+
+        "temperatura1": mainTemplate.enterTemper,
+        "temperatura2": finishTemp,
+        "jir1": mainTemplate.enterJir,
+        "jir2": mainTemplate.vihodJir,
+        "tempSec1": tempSec1,
+        "tempSec2": tempSec2,
+        "tempSec3": tempSec3,
+        "tempSec4": tempSec4,
+        "viderjTime": viderjTime,
+        "gomoDavlen": gomoDavlen,
+
+        "report_date": nowDate,
+
+        'user.name': reportObj.user.name,
+        'user.surname': reportObj.user.surname,
+        'user.group': reportObj.user.group
+    });
+
+    doc.render();
+
+    var buf = doc.getZip().generate({type:"nodebuffer"});
+
+    var dirPath = pathToStorage.join(nw.App.dataPath, 'userReports/');
+    if(!fs.existsSync(dirPath))
+        fs.mkdirSync(dirPath);
+    var fileName = 'Lab№2 '+reportObj.user.surname+' '+nowDate+' '+date.getTime()+'.docx';
+    var reportFile = 'userReports/'+fileName;
+    var newReportPath = pathToStorage.join(nw.App.dataPath, reportFile);
+
+    fs.open(newReportPath, 'wx+', function (err, fd) {
+        if (err) {
+            if (err.code === "EEXIST") {
+                console.log('myfile already exists');
+                return;
+            } else {
+                throw err;
+            }
+        }
+        fs.writeFileSync(newReportPath,buf);
+    });
+
+    user.reports.push(fileName);
+    users[user.login] = user;
+    var jsonResponseUsers = JSON.stringify(users);
+    fs.writeFileSync(pathToUsersFile,jsonResponseUsers);
+}
+
+var playButton = document.getElementById('playButton');
+function startApp() {
+    appIsStart = true;
+    playButton.style.display = 'none';
+    document.getElementById('timerDiv').style.display = 'block';
+    document.getElementById('startButton').style.display = 'block';
+    startTimer();
+    createLog('Начало работы');
+}
+function startTimer() {
+    if (init==0) {
+        startDate = new Date();
+        init=1;
+    }
+    var thisDate = new Date();
+    var t = thisDate.getTime() - startDate.getTime();
+    var ms = t%1000; t-=ms; ms=Math.floor(ms/10);
+    t = Math.floor (t/1000);
+    var s = t%60; t-=s;
+    t = Math.floor (t/60);
+    var m = t%60; t-=m;
+    t = Math.floor (t/60);
+    var h = t%60;
+    if (h<10) h='0'+h;
+    if (m<10) m='0'+m;
+    if (s<10) s='0'+s;
+    if (ms<10) ms='0'+ms;
+    if (init==1) document.getElementById('timer').innerText = h + ':' + m + ':' + s + '.' + ms;
+    timer = h + ':' + m + ':' + s + '.' + ms;
+    clocktimer = setTimeout("startTimer()",10);
 }
